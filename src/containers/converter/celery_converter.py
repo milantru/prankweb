cebee6003da7cab0e31d8b3fdf70d6425945e49c
Samuel Karaš <karikaras007@gmail.com>
2025-03-15 18:40:13 +0100

    Add celery endpoint for converter

--- src/containers/converter/celery_converter.py
+++ src/containers/converter/celery_converter.py
@@ -1,9 +1,13 @@
 import requests
 from Bio.PDB import PDBParser, Polypeptide
+from celery import Celery
+
+# Celery configuration
+celery = Celery('tasks', broker='amqp://guest:guest@message-broker:5672//', backend='rpc://')
 
 ESMFOLD_URL = "https://api.esmatlas.com/foldSequence/v1/pdb/"
 
-# TODO: inputs
+@celery.task(name='converter_str_to_seq')
 def structure_to_sequence(id, query_structure_file):
     with open(query_structure_file, "r") as file:
         pdb = PDBParser().get_structure(id, file) 
@@ -20,6 +24,7 @@ def structure_to_sequence(id, query_structure_file):
     
     return seq
 
+@celery.task(name='converter_seq_to_str')
 def sequence_to_structure(sequence):
     response = requests.post(ESMFOLD_URL, data=sequence)
     print(response.status_code)
