6320ea5abec340f8b4a703594bb2b75506ab7438
Bucková Katarína <katy.buckova@gmail.com>
2025-03-19 21:36:37 +0100

    convert multiple chains

--- src/containers/converter/celery_converter.py
+++ src/containers/converter/celery_converter.py
@@ -1,5 +1,5 @@
 import requests
-from Bio.PDB import PDBParser, Polypeptide
+from Bio.PDB import PDBParser, Polypeptide, is_aa
 
 ESMFOLD_URL = "https://api.esmatlas.com/foldSequence/v1/pdb/"
 
@@ -8,17 +8,16 @@ def structure_to_sequence(id, query_structure_file):
     with open(query_structure_file, "r") as file:
         pdb = PDBParser().get_structure(id, file) 
 
-    seq = ""
-
+    chains = {}
     for model in pdb:
         for chain in model:
+            seq = ""
             for residue in chain:
-                if residue.id[0] == " " or residue.id[0] == "": 
+                if (residue.id[0] == " " or residue.id[0] == "") and is_aa(residue): 
                     seq += Polypeptide.index_to_one(Polypeptide.three_to_index(residue.resname))
-            # TODO: multiple chains
-            # seq = seq + ":" + chain.id + " " 
+            chains[chain.id] = seq
     
-    return seq
+    return chains
 
 def sequence_to_structure(sequence):
     response = requests.post(ESMFOLD_URL, data=sequence)
