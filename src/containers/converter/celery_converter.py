85461a5bcc51d50fef863749787458625b7dd368
Samuel Karaš <karikaras007@gmail.com>
2025-03-27 14:08:45 +0100

    Add inputs for converter

--- src/containers/converter/celery_converter.py
+++ src/containers/converter/celery_converter.py
@@ -1,14 +1,28 @@
 import requests
 from Bio.PDB import PDBParser, Polypeptide, is_aa
 from celery import Celery
+import tempfile
 
 # Celery configuration
 celery = Celery('tasks', broker='amqp://guest:guest@message-broker:5672//', backend='rpc://')
 
 ESMFOLD_URL = "https://api.esmatlas.com/foldSequence/v1/pdb/"
+INPUTS_URL = "http://apache:80/inputs/"
 
 @celery.task(name='converter_str_to_seq')
-def structure_to_sequence(id, query_structure_file):
+def structure_to_sequence(id):
+    
+    pdb_url = INPUTS_URL + f"{id}/structure.pdb"
+    query_structure_file = ""
+
+    response = requests.get(pdb_url, stream=True)
+    response.raise_for_status()
+
+    with tempfile.NamedTemporaryFile(mode="wb", suffix=".pdb", delete=False) as struct_file:
+        for chunk in response.iter_content(chunk_size=8192):
+            struct_file.write(chunk)
+        query_structure_file = struct_file.name
+    
     with open(query_structure_file, "r") as file:
         pdb = PDBParser().get_structure(id, file) 
 
@@ -24,7 +38,14 @@ def structure_to_sequence(id, query_structure_file):
     return chains
 
 @celery.task(name='converter_seq_to_str')
-def sequence_to_structure(sequence):
+def sequence_to_structure(id):
+
+    # get input
+    fasta_url = INPUTS_URL + f"{id}/sequence.fasta"
+
+    response = requests.get(fasta_url)
+    sequence = response.text.split('\n')[1]
+
     response = requests.post(ESMFOLD_URL, data=sequence)
     print(response.status_code)
     if response.status_code == 200:
