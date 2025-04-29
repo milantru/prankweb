9f5793bb4d1c2e820e5aff0d6ee8cc244255770f
Samuel Karaš <karikaras007@gmail.com>
2025-04-29 13:20:48 +0200

    Pip warnings removed, change timezone of flower back to UTC

--- src/containers/shared/install-requirements.sh
+++ src/containers/shared/install-requirements.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-pip install --no-cache-dir pip-tools
+pip install --root-user-action=ignore --no-cache-dir pip-tools
 pip-compile requirements.in
-pip install --no-cache-dir -r requirements.txt
-pip uninstall -y pip-tools
+pip install --root-user-action=ignore --no-cache-dir -r requirements.txt
+pip uninstall --root-user-action=ignore -y pip-tools
