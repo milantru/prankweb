72dca232744f2d77f0253fcb98825e1828ae2d6f
RichardF <richardfedak@gmail.com>
2025-05-09 14:44:59 +0200

    Add get-id tests

--- testing/plankweb-tests.py
+++ testing/plankweb-tests.py
@@ -2,7 +2,10 @@ import json
 import pytest
 import requests
 
-API_URL = "http://localhost:80/upload-data"
+SERVER_URL = "http://localhost:80"
+
+UPLOAD_DATA_URL = f"{SERVER_URL}/upload-data"
+ID_PROVIDER_URL = f"{SERVER_URL}/get-id"
 
 with open("tests/sequence.json") as f1, open("tests/alphafold_structure.json") as f2, open("tests/experimental_structure.json") as f3, open("tests/custom_structure.json") as f4, open("tests/invalid_input_method.json") as f5:
     sequence_cases = json.load(f1)
@@ -10,10 +13,12 @@ with open("tests/sequence.json") as f1, open("tests/alphafold_structure.json") a
     pdb_cases = json.load(f3)
     custom_str_cases = json.load(f4)
     invalid_input_method_cases = json.load(f5)
-    test_cases =  sequence_cases + uniprot_cases + pdb_cases + custom_str_cases + invalid_input_method_cases
+    test_cases_upload_data =  sequence_cases + uniprot_cases + pdb_cases + custom_str_cases + invalid_input_method_cases
 
+with open("tests/get_id.json") as f1:
+    test_cases_get_id = json.load(f1)
 
-@pytest.mark.parametrize("case", test_cases, ids=[tc["description"] for tc in test_cases])
+@pytest.mark.parametrize("case", test_cases_upload_data, ids=[tc["description"] for tc in test_cases_upload_data])
 def test_plankweb_post_request(case):
     payload = case["payload"]
     expected_result = case["result"]
@@ -26,11 +31,11 @@ def test_plankweb_post_request(case):
         with open(payload['userFile']) as f:
             files = {'userFile': (payload['userFile'], f)}
             del payload['userFile']
-            response = requests.post(API_URL, data=payload, files=files)
+            response = requests.post(UPLOAD_DATA_URL, data=payload, files=files)
             actual_result = response.json()
     else:
         # Experimental structure, AlphaFold structure, Sequence 
-        response = requests.post(API_URL, data=payload)
+        response = requests.post(UPLOAD_DATA_URL, data=payload)
         actual_result = response.json()
 
 
@@ -42,3 +47,28 @@ def test_plankweb_post_request(case):
     else:
         assert isinstance(actual_result, str)
         assert actual_result.startswith(expected_result), f"{description}: Mismatch\nExpected: {expected_result}\nGot: {actual_result}"
+
+
+@pytest.mark.parametrize("case", test_cases_get_id, ids=[tc["description"] for tc in test_cases_get_id])
+def test_plankweb_get_id(case):
+    payload = case["payload"]
+    expected_result = case["result"]
+    description = case["description"]
+
+    actual_result = None
+     
+    response = requests.get(ID_PROVIDER_URL, params=payload)
+    actual_result = response.json()
+
+   # assert response.status_code == 200, f"{description}: Unexpected status code {response.status_code}"
+
+    if "id" in expected_result:
+        if expected_result["id"] is None:
+            assert isinstance(actual_result, dict)
+            assert actual_result["id"] is None, f"{description}: Mismatch\nExpected: {expected_result}\nGot: {actual_result}"
+        else:
+            assert isinstance(actual_result, dict)
+            assert actual_result["id"].startswith(expected_result["id"]), f"{description}: Mismatch\nExpected: {expected_result}\nGot: {actual_result}"
+    else: # Error
+        assert isinstance(actual_result, dict)
+        assert actual_result["error"].startswith(expected_result["error"]), f"{description}: Mismatch\nExpected: {expected_result}\nGot: {actual_result}"
