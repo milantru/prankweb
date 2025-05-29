22cad7dd7c1cb47f44c514f52bb08b80a3b396da
RichardF <richardfedak@gmail.com>
2025-05-29 11:06:13 +0200

    Shared status

--- src/containers/data-source-executors/executor-foldseek/foldseek_executor.py
+++ src/containers/data-source-executors/executor-foldseek/foldseek_executor.py
@@ -6,30 +6,18 @@ from enum import Enum
 import post_processor
 
 from tasks_logger import create_logger
+from status_manager import update_status, StatusType
 
 RESULTS_FOLDER = "results"
 FOLDSEEK_DB = "foldseek_db/pdb"
 INPUTS_URL = os.getenv('INPUTS_URL')
 PLANKWEB_BASE_URL = os.getenv('PLANKWEB_BASE_URL')
 
-class StatusType(Enum):
-    STARTED = 0
-    COMPLETED = 1
-    FAILED = 2
 
 logger = create_logger('ds-foldseek')
 
 os.makedirs(RESULTS_FOLDER, exist_ok=True)
 logger.info(f'{id} Results folder prepared: {RESULTS_FOLDER}')
-
-def update_status(status_file_path, id, status, message=""):
-    logger.info(f'{id} Changing status in {status_file_path} to: {status}')
-    try:
-        with open(status_file_path, "w") as f:
-            json.dump({"status": status, "errorMessages": message}, f)
-        logger.info(f'{id} Status changed')
-    except Exception as e:
-        logger.error(f'{id} Status change failed: {str(e)}')
 
 def run_foldseek(id):
     logger.info(f'{id} ds_foldseek started')
@@ -38,7 +26,7 @@ def run_foldseek(id):
     os.makedirs(eval_folder, exist_ok=True)
     logger.info(f'{id} Evaluation folder created: {eval_folder}')
     status_file_path = os.path.join(eval_folder, "status.json")
-    update_status(status_file_path, id, StatusType.STARTED.value)
+    update_status(status_file_path, id, StatusType.STARTED, infoMessage="Execution started")
 
     try:
         pdb_url = os.path.join(INPUTS_URL, id, "structure.pdb")
@@ -74,6 +62,8 @@ def run_foldseek(id):
             "structure.pdb"
         )
 
+        update_status(status_file_path, id, StatusType.STARTED, infoMessage="Processing Foldseek output")
+
         post_processor.process_foldseek_output(
             eval_folder,
             foldseek_result_file,
@@ -82,18 +72,18 @@ def run_foldseek(id):
             query_structure_url
         )
 
-        update_status(status_file_path, id, StatusType.COMPLETED.value)
+        update_status(status_file_path, id, StatusType.COMPLETED, infoMessage="Execution completed successfully")
 
     except requests.RequestException as e:
         logger.error(f'{id} PDB file download failed: {str(e)}')
-        update_status(status_file_path, id, StatusType.FAILED.value, f"Failed to download PDB file: {str(e)}")
+        update_status(status_file_path, id, StatusType.FAILED, errorMessage = f"Failed to download PDB file: {str(e)}")
     except subprocess.CalledProcessError as e:
         err_msg = e.stderr.decode() # error message is from subprocess, needs different treatment
         logger.error(f'{id} Foldseek subprocess crashed: {err_msg}')
-        update_status(status_file_path, id, StatusType.FAILED.value, f"Foldseek crashed: {err_msg}")
+        update_status(status_file_path, id, StatusType.FAILED, errorMessage = f"Foldseek crashed: {err_msg}")
     except Exception as e:
         logger.error(f'{id} An unexpected error occurred ({type(e).__name__}): {str(e)}')
-        update_status(status_file_path, id, StatusType.FAILED.value, f"An unexpected error occurred: {str(e)}")
+        update_status(status_file_path, id, StatusType.FAILED, errorMessage = f"An unexpected error occurred: {str(e)}")
 
     logger.info(f'{id} ds_foldseek finished')
 
