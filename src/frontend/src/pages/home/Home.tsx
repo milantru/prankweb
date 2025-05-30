1d9388e1df913eabd051f37faabe08b2c2b67025
RichardF <richardfedak@gmail.com>
2025-05-30 17:02:49 +0200

    New logo

--- src/frontend/src/pages/home/Home.tsx
+++ src/frontend/src/pages/home/Home.tsx
@@ -6,10 +6,10 @@ function Home() {
 		<>
 			<div className="container index-view" style={{ marginTop: "3rem" }}>
 				<h1 className="text-center">
-					Plankweb: Ligand Binding Site Analysis
+					PlankWeb: Ligand Binding Site Analysis
 				</h1>
 				<p className="text-center description"> 
-					Plankweb integrates diverse data sources and computational methods to enhance the analysis of 
+					PlankWeb integrates diverse data sources and computational methods to enhance the analysis of 
 					protein structures and the prediction of ligand binding sites. 
 				</p>
 
