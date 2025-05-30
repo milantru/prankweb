3eddc862513c0656f2aa3b0074b92fee1f631449
RichardF <richardfedak@gmail.com>
2025-05-30 16:43:44 +0200

    Update description

--- src/frontend/src/pages/home/Home.tsx
+++ src/frontend/src/pages/home/Home.tsx
@@ -6,11 +6,11 @@ function Home() {
 		<>
 			<div className="container index-view" style={{ marginTop: "3rem" }}>
 				<h1 className="text-center">
-					PlankWeb: Ligand Binding Site Analysis
+					Plankweb: Ligand Binding Site Analysis
 				</h1>
-				<p className="text-center description">
-					PlankWeb builds upon P2Rank and pLM&nbsp;‐ a machine and deep learning-based methods for prediction of
-					ligand binding sites from protein structure.
+				<p className="text-center description"> 
+					Plankweb integrates diverse data sources and computational methods to enhance the analysis of 
+					protein structures and the prediction of ligand binding sites. 
 				</p>
 
 				<QueryProteinForm />
