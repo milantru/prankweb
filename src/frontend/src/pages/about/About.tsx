b1e39d856b0eeb9555606334ad4062ff151d4947
milantru1 <milantru1@gmail.com>
2025-06-02 20:55:09 +0200

    Update About page (mostly content order)

--- src/frontend/src/pages/about/About.tsx
+++ src/frontend/src/pages/about/About.tsx
@@ -18,16 +18,15 @@ function About() {
                 and convenient way for scientists to analyze proteins.
             </p>
 
+            <h2 id="structure-prediction">Structure prediction</h2>
+            <p>
+                For sequence input, the application uses the <a href="https://esmatlas.com/resources?action=fold" target="_blank" rel="noopener noreferrer">ESMFold&nbsp;API&nbsp;</a>
+                to predict the 3D structure of the provided protein sequence.
+            </p>
+
             <h2 id="data-sources">Data sources</h2>
             <p>
                 The application uses the following data sources:
-            </p>
-
-            <h3 id="p2rank">P2Rank</h3>
-            <p>
-                <a href="https://github.com/cusbg/p2rank-framework" target="_blank" rel="noopener noreferrer">P2Rank</a> is
-                a machine learning-based method for predicting ligand-binding pockets in proteins.
-                In the application, the model is used to predict which residues are part of specific binding sites.
             </p>
 
             <h3 id="esm2">ESM-2</h3>
@@ -38,6 +37,13 @@ function About() {
                 a residue is part of a binding site without needing any information about the protein’s 3D structure.
             </p>
 
+            <h3 id="p2rank">P2Rank</h3>
+            <p>
+                <a href="https://github.com/cusbg/p2rank-framework" target="_blank" rel="noopener noreferrer">P2Rank</a> is
+                a machine learning-based method for predicting ligand-binding pockets in proteins.
+                In the application, the model is used to predict which residues are part of specific binding sites.
+            </p>
+
             <h3 id="foldseek">Foldseek</h3>
             <p>
                 <a href="https://github.com/steineggerlab/foldseek" target="_blank" rel="noopener noreferrer">Foldseek</a> is a
@@ -45,14 +51,6 @@ function About() {
                 supporting monomer and multimer searches, as well as clustering. We use it with the <strong>PDB100 database</strong> (100% sequence identity clustered PDB) to get
                 the proteins most similar to the submitted protein, along with their associated data.
             </p>
-
-            <h2 id="structure-prediction">Structure prediction</h2>
-            <p>
-                For sequence input, the application uses the <a href="https://esmatlas.com/resources?action=fold" target="_blank" rel="noopener noreferrer">ESMFold&nbsp;API&nbsp;</a>
-                to predict the 3D structure of the provided protein sequence.
-            </p>
-
-            {/* <Footer /> */}
         </div>
     );
 }
