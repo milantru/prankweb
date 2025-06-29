21242e75d781f549634f0148be8e5d494df822d4
Bucková Katarína <katy.buckova@gmail.com>
2025-06-29 22:27:09 +0200

    extend evaluation

--- src/containers/data-source-executors/executor-plank/evaluate.py
+++ src/containers/data-source-executors/executor-plank/evaluate.py
@@ -1,8 +1,9 @@
 import numpy as np
 import torch
 import pandas as pd
+import matplotlib.pyplot as plt
 from model import BindingPredictor
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, matthews_corrcoef
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, matthews_corrcoef, roc_curve, RocCurveDisplay, auc, precision_recall_curve
 import pickle
 import os
 from model import BindingPredictor
@@ -35,11 +36,11 @@ def evaluate(model, file_name, embeddings_dir):
             labels =  torch.tensor(chain_info["LIGYSIS"], dtype=torch.float32)
             if len(labels) == embeddings.shape[0]:
                 embeddings, labels = embeddings.to(device, non_blocking=True), labels.to(device, non_blocking=True)
-                outputs = model(embeddings).squeeze()
 
-                predictions = (torch.sigmoid(outputs) > model.pred_threshold).float()
+                with torch.no_grad():
+                    outputs = model(embeddings).squeeze()
+                    predictions = torch.sigmoid(outputs).float()
 
-                correct += (predictions == labels).sum().item()
                 all_preds.extend(predictions.cpu().numpy())
                 all_labels.extend(labels.cpu().numpy())
                 p2rank_labels.extend(np.array(chain_info["P2Rank"], dtype=np.float32))
@@ -48,8 +49,9 @@ def evaluate(model, file_name, embeddings_dir):
             else:
                 print(f"⚠️ Mismatch: {chain} (Residues: {len(labels)}, Embeddings: {embeddings.shape[0]})")
 
-
-    for pred, model in zip([all_preds, p2rank_labels, cp2rank_labels], ["", "P2Rank:", "P2Rank+Cons:"]):
+    print()
+    # calculate P2Rank metrics
+    for pred, model in zip([p2rank_labels, cp2rank_labels], ["P2Rank:", "P2Rank+Cons:"]):
         test_accuracy = accuracy_score(all_labels, pred)
         test_precision = precision_score(all_labels, pred, zero_division=0)
         test_recall = recall_score(all_labels, pred, zero_division=0)
@@ -57,6 +59,37 @@ def evaluate(model, file_name, embeddings_dir):
         test_mcc = matthews_corrcoef(all_labels, pred)
 
         print(f"{model} Accuracy: {test_accuracy:.4f} | Precision: {test_precision:.4f} | Recall: {test_recall:.4f} | F1: {test_f1:.4f} | MCC: {test_mcc:.4f}")
+    
+    # evaluate model predictions for different thresholds
+    print("\nPlank:")
+    for i in range(1, 10):
+        threshold = i / 10.0
+        pred = np.array(all_preds) > threshold
+        test_accuracy = accuracy_score(all_labels, pred)
+        test_precision = precision_score(all_labels, pred, zero_division=0)
+        test_recall = recall_score(all_labels, pred, zero_division=0)
+        test_f1 = f1_score(all_labels, pred, zero_division=0)
+        test_mcc = matthews_corrcoef(all_labels, pred)
+        print(f"Threshold: {threshold} - Accuracy: {test_accuracy:.4f} | Precision: {test_precision:.4f} | Recall: {test_recall:.4f} | F1: {test_f1:.4f} | MCC: {test_mcc:.4f}")
+
+    # plot ROC
+    fpr, tpr, thresholds = roc_curve(all_labels, all_preds)
+    roc_auc = auc(fpr, tpr)
+    print(f"ROC AUC: {roc_auc:.4f}")
+    display = RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=roc_auc)
+    display.plot()
+    plt.savefig('plank_roc.png')
+
+    # plot precision-recall curve
+    plt.clf()
+    precisions, recalls, thresholds = precision_recall_curve(all_labels, all_preds)
+    plt.plot(thresholds, precisions[:-1], label="Precision")
+    plt.plot(thresholds, recalls[:-1], label="Recall")
+    plt.xlabel('Prediction Threshold')
+    plt.ylabel('Score')
+    plt.legend()
+    plt.title('Precision and Recall Scores based on Prediction Threshold')
+    plt.savefig('plank_pr.png')
 
 model_path = "models/model_e10.pth"
 test_path = "data/LIGYSIS_predictions.pkl"
